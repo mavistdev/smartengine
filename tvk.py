dbe74f4fb7b66dd8eb1ca86fed24dfc13efab67f
Ivan <107255861+mavistdev@users.noreply.github.com>
2023-07-15 11:10:24 +0300

    Update tvk.py

--- tvk.py
+++ tvk.py
@@ -5,6 +5,6 @@ bot = telebot.TeleBot('')
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
 	print(message.text)
-	requests.post('https://discord.com/api/webhooks/1064086850823798804/wbErnY1izmxVKGI6tg9R4MEO22vPtTb1Jn1yuXGve6f8097pdwRElrBK6uMmsd22lg01', {"content": message.text})
+	requests.post('', {"content": message.text})
 
 bot.polling(none_stop=True, interval=0)
